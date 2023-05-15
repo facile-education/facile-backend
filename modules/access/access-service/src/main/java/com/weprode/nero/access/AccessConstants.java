dd887b02e3dfbf0afa22e3a772be16098cdfbbe3
Vivien Donat-Bouillud <vivien.donat-bouillud@pentila.com>
2023-05-15 10:35:53 +0200

    [Accesses] Rename accessList label for returned JSON

--- modules/access/access-service/src/main/java/com/weprode/nero/access/AccessConstants.java
+++ modules/access/access-service/src/main/java/com/weprode/nero/access/AccessConstants.java
@@ -6,7 +6,7 @@ public class AccessConstants {
     public static final String CATEGORY_NAME = "categoryName";
     public static final String POSITION = "position";
 
-    public static final String ACCESSES = "acces";
+    public static final String ACCESSES = "accessList";
     public static final String ACCESS_ID = "accessId";
     public static final String TITLE = "title";
     public static final String URL = "url";
