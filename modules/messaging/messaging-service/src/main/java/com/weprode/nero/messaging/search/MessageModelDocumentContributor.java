1a5411d9f7b08d00785b0a66c2f2c213bcdf071d
Marc Salvat <marc.salvat@weprode.com>
2023-05-31 15:02:14 +0200

    Update indexers : add roleId and keep accented characters.

--- modules/messaging/messaging-service/src/main/java/com/weprode/nero/messaging/search/MessageModelDocumentContributor.java
+++ modules/messaging/messaging-service/src/main/java/com/weprode/nero/messaging/search/MessageModelDocumentContributor.java
@@ -4,10 +4,13 @@ import com.liferay.document.library.kernel.service.DLAppServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.util.HtmlParserUtil;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.weprode.nero.messaging.model.Message;
@@ -17,7 +20,6 @@ import com.weprode.nero.messaging.service.MessageContentLocalServiceUtil;
 import com.weprode.nero.messaging.service.MessageFolderLocalServiceUtil;
 import org.osgi.service.component.annotations.Component;
 
-import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,32 +34,30 @@ public class MessageModelDocumentContributor
     @Override
     public void contribute(Document document, Message message) {
         try {
-            // titre de message,
-            // contenu de message,
-            // expéditeur,
-            // nom des pièces jointes,
-            //TODO contenu textuel des pièces jointes
             MessageContent content = MessageContentLocalServiceUtil.getMessageContent(message.getMessageId());
 
             document.addDate(Field.MODIFIED_DATE, message.getSendDate());
-            document.addText(Field.TITLE, normalize(message.getMessageSubject()));
+            document.addText(Field.TITLE, message.getMessageSubject());
             document.addNumber(Field.COMPANY_ID, message.getCompanyId());
             User owner = MessageFolderLocalServiceUtil.getFolderUser(message.getFolderId());
             document.addNumber(Field.GROUP_ID, owner.getGroupId());
             document.addNumber(Field.SCOPE_GROUP_ID, owner.getGroupId());
 
-            document.addText(Field.CONTENT, normalize(HtmlParserUtil.extractText(content.getMessageContent())));
+            Role userRole = RoleLocalServiceUtil.getRole(message.getCompanyId(), RoleConstants.USER);
+            document.addText(Field.ROLE_ID, new String[]{String.valueOf(userRole.getRoleId())});
+
+            document.addText(Field.CONTENT, HtmlParserUtil.extractText(content.getMessageContent()));
             document.addDate(Field.DISPLAY_DATE, message.getSendDate());
             document.addNumber(Field.FOLDER_ID, message.getFolderId());
             document.addNumber(Field.USER_ID, message.getSenderId());
-            document.addText(Field.USER_NAME, normalize(message.getSenderName()));
+            document.addText(Field.USER_NAME, message.getSenderName());
             document.addNumber(Field.TYPE, message.getType());
 
             List<String> fileNames = new ArrayList<>();
 
             for (Long attachmentId : MessageAttachFileLocalServiceUtil.getMessageAttachFileIds(message.getMessageId())) {
                 FileEntry attachment = DLAppServiceUtil.getFileEntry(attachmentId);
-                fileNames.add(normalize(attachment.getTitle()));
+                fileNames.add(attachment.getTitle());
             }
 
             String[] attachmentNames = fileNames.toArray(String[]::new);
@@ -67,10 +67,6 @@ public class MessageModelDocumentContributor
         } catch (PortalException e) {
             logger.warn("Unable to index message " + message.getMessageId(), e);
         }
-    }
-
-    private String normalize(String str) {
-        return Normalizer.normalize(str, Normalizer.Form.NFKD).replaceAll("\\p{M}", "");
     }
 
     private static final Log logger = LogFactoryUtil.getLog(
