2a492f56e3596ca7012d6d2ae0767a1b47a9fbe4
Marc Salvat <marc.salvat@weprode.com>
2023-07-17 11:38:45 +0200

    Add missing login statistic.

---
+++ modules/authentication/authentication-service/src/main/java/com/weprode/nero/authentication/LoginPostAction.java
@@ -0,0 +1,37 @@
+package com.weprode.nero.authentication;
+
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.events.LifecycleEvent;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.weprode.nero.statistic.service.UserLoginLocalServiceUtil;
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+        immediate = true, property = "key=login.events.post",
+        service = LifecycleAction.class
+)
+public class LoginPostAction implements LifecycleAction {
+
+    private static final Log logger = LogFactoryUtil.getLog(LoginPostAction.class);
+
+    @Override
+    public void processLifecycleEvent(LifecycleEvent lifecycleEvent)
+            throws ActionException {
+
+        try {
+            User identifiedUser = PortalUtil.getUser(lifecycleEvent.getRequest());
+
+            logger.debug("Adding login stat for user " + identifiedUser.getFullName());
+            UserLoginLocalServiceUtil.addUserLogin(identifiedUser, false);
+        } catch (PortalException e) {
+            logger.error("Could not add login stat for logged user", e);
+        }
+
+    }
+
+}
