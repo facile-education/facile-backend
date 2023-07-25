e0e55712848abd049add27d505f9ce3d65e3d450
Cedric <cedric.lecarpentier@weprode.com>
2023-07-25 11:26:37 +0200

    #4266 - Shibboleth AutoLogin + CustomAuthVerifier

---
+++ modules/authentication/authentication-service/src/main/java/com/weprode/nero/authentication/CustomAuthVerifier.java
@@ -0,0 +1,53 @@
+package com.weprode.nero.authentication;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.AccessControlContext;
+import com.liferay.portal.kernel.security.auth.verifier.AuthVerifier;
+import com.liferay.portal.kernel.security.auth.verifier.AuthVerifierResult;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Properties;
+
+@Component(
+        immediate = true,
+        service = AuthVerifier.class,
+        property = {
+                "auth.verifier.CustomAuthVerifier.urls.includes=*"
+        }
+)
+public class CustomAuthVerifier implements AuthVerifier {
+
+    private static final Log logger = LogFactoryUtil.getLog(CustomAuthVerifier.class);
+
+    @Override
+    public String getAuthType() {
+        return HttpServletRequest.FORM_AUTH;
+    }
+
+    @Override
+    public AuthVerifierResult verify(AccessControlContext accessControlContext, Properties properties) {
+        try {
+            String[] credentials = new ShibbolethAutoLogin().login(accessControlContext.getRequest(), accessControlContext.getResponse());
+            AuthVerifierResult authVerifierResult = new AuthVerifierResult();
+            if (credentials != null) {
+                long userId = Long.parseLong(credentials[0]);
+                User user = UserLocalServiceUtil.getUser(userId);
+                logger.info("USER connected is " + user.getFullName());
+                authVerifierResult.setState(AuthVerifierResult.State.SUCCESS);
+                authVerifierResult.setPasswordBasedAuthentication(true);
+                authVerifierResult.setUserId(userId);
+                authVerifierResult.setPassword(credentials[1]);
+            } else {
+                authVerifierResult.setState(AuthVerifierResult.State.INVALID_CREDENTIALS);
+            }
+            return authVerifierResult;
+        } catch (Exception e) {
+            logger.error("Exception in AuthVerifier");
+        }
+        return null;
+    }
+}
