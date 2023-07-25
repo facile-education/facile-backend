601d90083797087ec132c2442eae6648060f7c8f
Cedric <cedric.lecarpentier@weprode.com>
2023-07-25 11:26:37 +0200

    #4266 - Shibboleth AutoLogin + CustomAuthVerifier

--- modules/authentication/authentication-service/src/main/java/com/weprode/nero/authentication/ShibbolethAutoLogin.java
+++ modules/authentication/authentication-service/src/main/java/com/weprode/nero/authentication/ShibbolethAutoLogin.java
@@ -1,7 +1,6 @@
 package com.weprode.nero.authentication;
 
 import com.liferay.portal.kernel.exception.NoSuchUserException;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.CompanyConstants;
@@ -17,44 +16,48 @@ import org.osgi.service.component.annotations.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Component(immediate = true)
 public class ShibbolethAutoLogin implements AutoLogin {
 
     private static final Log logger = LogFactoryUtil.getLog(ShibbolethAutoLogin.class);
 
-    public String[] handleException(
-            HttpServletRequest request, HttpServletResponse response,
-            Exception e)
-            throws AutoLoginException {
-        // Taken from BaseAutoLogin
-        if (Validator.isNull(request.getAttribute(AutoLogin.AUTO_LOGIN_REDIRECT))) {
-            throw new AutoLoginException(e);
-        }
-        logger.error("Error in Shibboleth Auto Login", e);
-        return null;
-    }
-
     public String[] login(
             HttpServletRequest request, HttpServletResponse response)
             throws AutoLoginException {
 
-        User user;
-        String[] credentials = null;
-        HttpSession session = request.getSession(false);
-        long companyId = PortalUtil.getCompanyId(request);
-
-
         try {
-            logger.info("Shibboleth Autologin");
+            String authType = PrefsPropsUtil.getString(PropsKeys.COMPANY_SECURITY_AUTH_TYPE, CompanyConstants.AUTH_TYPE_EA);
+            long companyId = PortalUtil.getCompanyId(request);
+            User user = null;
 
-            user = loginFromSession(companyId, session);
-            if (Validator.isNotNull(user)) {
-                credentials = new String[3];
+            if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
+                String login = request.getHeader(SHIBBOLETH_NAME_ID);
+                logger.debug("AuthType is SN : login=" + login);
+                if (Validator.isNull(login)) {
+                    return null;
+                }
+                logger.debug("Trying to find user with screen name: " + login);
+                user = UserLocalServiceUtil.getUserByScreenName(companyId, login);
+
+            } else if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
+
+                String emailAddress = request.getHeader(SHIBBOLETH_EMAIL);
+                logger.debug("AuthType is Email : mail=" + emailAddress);
+                if (Validator.isNull(emailAddress)) {
+                    return null;
+                }
+                logger.debug("Trying to find user with email: " + emailAddress);
+                user = UserLocalServiceUtil.getUserByEmailAddress(companyId, emailAddress);
+            }
+
+            if (user != null) {
+                logger.info("User authenticated: " + user.getScreenName() + " (" + user.getEmailAddress() + ")");
+                String[] credentials = new String[3];
                 credentials[0] = String.valueOf(user.getUserId());
                 credentials[1] = user.getPassword();
-                credentials[2] = Boolean.TRUE.toString();
+                credentials[2] = String.valueOf(user.isPasswordEncrypted());
+                return credentials;
             }
 
         } catch (NoSuchUserException e) {
@@ -64,46 +67,10 @@ public class ShibbolethAutoLogin implements AutoLogin {
             throw new AutoLoginException(e);
         }
 
-        return credentials;
+        return null;
     }
 
-    private User loginFromSession(long companyId, HttpSession session) throws PortalException {
-
-        User user = null;
-        if (session == null) {
-            return null;
-        }
-        String authType = PrefsPropsUtil.getString(PropsKeys.COMPANY_SECURITY_AUTH_TYPE, CompanyConstants.AUTH_TYPE_EA);
-
-        try {
-            if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
-                String login = (String) session.getAttribute(SHIBBOLETH_LOGIN);
-                if (Validator.isNull(login)) {
-                    return null;
-                }
-                logger.info("Trying to find user with screen name: " + login);
-                user = UserLocalServiceUtil.getUserByScreenName(companyId, login);
-
-            } else if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
-
-                String emailAddress = (String) session.getAttribute(SHIBBOLETH_HEADER_EMAIL);
-                if (Validator.isNull(emailAddress)) {
-                    return null;
-                }
-                logger.info("Trying to find user with email: " + emailAddress);
-                user = UserLocalServiceUtil.getUserByEmailAddress(companyId, emailAddress);
-            }
-
-            logger.info("User found: " + user.getScreenName() + " (" + user.getEmailAddress() + ")");
-
-        } catch (NoSuchUserException e) {
-            logger.error("User not found", e);
-        }
-
-        return user;
-    }
-
-    public static final String SHIBBOLETH_HEADER_EMAIL = "mail";
-    public static final String SHIBBOLETH_LOGIN = "shibboleth.login";
+    public static final String SHIBBOLETH_NAME_ID = "NameID";
+    public static final String SHIBBOLETH_EMAIL = "mail";
 
 }
