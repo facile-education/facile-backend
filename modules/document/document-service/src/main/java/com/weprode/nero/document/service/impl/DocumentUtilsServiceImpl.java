a39c0702a5ef751acb916eddbcba7259b460799e
Marc Salvat <marc.salvat@weprode.com>
2023-10-12 15:36:18 +0200

    Fix date format typo issue.

--- modules/document/document-service/src/main/java/com/weprode/nero/document/service/impl/DocumentUtilsServiceImpl.java
+++ modules/document/document-service/src/main/java/com/weprode/nero/document/service/impl/DocumentUtilsServiceImpl.java
@@ -130,7 +130,7 @@ public class DocumentUtilsServiceImpl extends DocumentUtilsServiceBaseImpl {
 
 		try {
 			JSONArray jsonActivities = new JSONArray();
-			Date maximumDate = new SimpleDateFormat("yyyy-MM-dd+HH:mm:ss").parse(maxDate);
+			Date maximumDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(maxDate);
 			List<GroupActivity> groupActivities = GroupActivityLocalServiceUtil.getDocumentGroupActivities(user.getUserId(), groupId, maximumDate, nbResults);
 			for (GroupActivity groupActivity : groupActivities) {
 				JSONObject jsonActivity = GroupActivityLocalServiceUtil.convertGroupActivity(user.getUserId(), groupActivity);
