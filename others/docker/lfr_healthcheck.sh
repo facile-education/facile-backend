037361c71431f819eb356372be8ae91d1a9a24ff
Marc Salvat <marc.salvat@weprode.com>
2023-11-30 10:27:44 +0100

    Fix liferay docker healthcheck.

--- others/docker/lfr_healthcheck.sh
+++ others/docker/lfr_healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-http_validation=$(curl -s -o /dev/null -w "%{http_code}" https://dev-ent-gve.com/lfr/api/jsonws/user.userutils/get-user-infos?p_auth=0 --insecure)
+http_validation=$(curl -s -o /dev/null -w "%{http_code}" 127.0.0.1:8080/lfr/api/jsonws/user.userutils/get-user-infos?p_auth=0 --insecure)
 
 if [[ $http_validation == 200 || $http_validation == "403" ]]
 then
