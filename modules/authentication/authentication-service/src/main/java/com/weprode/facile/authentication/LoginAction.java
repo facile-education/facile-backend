cab4609f9ee68a0caa2ac02cd7f75ed6d211471d
Cedric <cedric.lecarpentier@weprode.com>
2024-01-09 19:18:19 +0100

    Fix login failures count display

--- modules/authentication/authentication-service/src/main/java/com/weprode/facile/authentication/LoginAction.java
+++ modules/authentication/authentication-service/src/main/java/com/weprode/facile/authentication/LoginAction.java
@@ -100,7 +100,8 @@ public class LoginAction implements StrutsAction {
             LoginLockLocalServiceUtil.addLoginAttempt(login);
 
             LoginLock loginLock = LoginLockLocalServiceUtil.getLoginLock(login);
-            int nbRemainingTries = PasswordPolicyLocalServiceUtil.getDefaultPasswordPolicy(PortalUtil.getDefaultCompanyId()).getMaxFailure() - loginLock.getFailedLoginAttempts();
+            int passwordPolicyMaxFailures = PasswordPolicyLocalServiceUtil.getDefaultPasswordPolicy(PortalUtil.getDefaultCompanyId()).getMaxFailure();
+            int nbRemainingTries = passwordPolicyMaxFailures >= loginLock.getFailedLoginAttempts() ? passwordPolicyMaxFailures - loginLock.getFailedLoginAttempts() : 0;
             userStatus.put("nbRemainingTries", nbRemainingTries);
             if (nbRemainingTries <= 0) {
                 logger.error("False user is locked");
